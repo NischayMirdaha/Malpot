3948019242cab7465c7d9c0358424fd1915fa1e8
NischayMirdaha <nischaymirdaha@email.com>
2026-01-02 23:07:08 +0545

    first commit

---
+++ Backend/server.js
@@ -0,0 +1,28 @@
+import express from "express"
+import connectDB from "./config/mongodb.js"; //import from the mongodb.js 
+
+
+import cors from "cors"
+
+import dotenv from "dotenv"
+dotenv.config();
+
+
+const app=express();
+const PORT= process.env.PORT || 3000;
+
+//
+app.use(cors())
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(express.static("./storage"));
+
+connectDB()//call the function from the mongodb.js
+
+//Routes
+
+
+
+app.listen(PORT, ()=>{
+    console.log(`Server is running on  the port ${PORT}`)   
+})
